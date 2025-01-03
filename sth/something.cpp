dd1c9370943af4abf42146068d64a902b4ecb0ed
Jemajr <joshua.ma.udo@gmail.com>
2025-01-03 02:32:36 -0800

    Using Shared Pointers

--- sth/something.cpp
+++ sth/something.cpp
@@ -10,7 +10,7 @@ class Something {
     ~Something(){cout << "Deallocated Something..." << endl;} // destructor
 
   private:
-    int thing = 1; // I still don't get why we have this set to 1. We reset it to 0 above
+    int thing = 1;
 };
 
 int main(){
@@ -19,6 +19,11 @@ int main(){
   up1->getThing(); // just testing. this prints 0
 
   unique_ptr<Something> up2 = make_unique<Something>(); // different syntax. Doesn't use 'new' keyword and can handle exceptions
-  
+
+  // making shared pointers instead of unique pointers so they can point to the same piece of memory
+  shared_ptr<Something> sp1 = make_shared<Something>();
+  shared_ptr<Something> sp2 = sp1; // sp2 points to the same address as sp1. Interesting stuff!
+
+
   return 0;
 }
